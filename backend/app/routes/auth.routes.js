dc56f56727c41cf32b596d9d459c4b8def038aff
Lussmann Sven (L9S) <sven.lussmann@suva.ch>
2023-08-27 00:40:38 +0200

    v45

--- backend/app/routes/auth.routes.js
+++ backend/app/routes/auth.routes.js
@@ -1,14 +1,14 @@
 const { verifySignUp } = require("../middlewares");
-const AuthController = require("../controllers/authController/index"); // Import the exported functions
-const User = require("../models/user.model"); // Replace with the correct path to your User model
-const Role = require("../models/role.model"); // Replace with the correct path to your Role model
+const AuthController = require("../controllers/authController/index"); 
+const User = require("../models/user.model"); 
+const Role = require("../models/role.model");
 
 module.exports = function (app) {
   app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Headers", "Origin, Content-Type, Accept");
     next();
   });
-  const authController = AuthController(User, Role); // Initialize the authentication functions
+  const authController = AuthController(User, Role);
 
   const base_url = "/api/auth";
 
